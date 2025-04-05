62a833fa44d36c6be41ac0fa3127de1a5c901f00
LilianBarahona <lilianlisbethbarahonagomez@gmail.com>
2025-04-04 19:05:23 -0600

    Corregir configuración base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+const DEPLOY_BASE_URL = process.env.DEPLOY_BASE_URL || '';
 
 // https://vite.dev/config/
 export default defineConfig({
-  comobase: process.env.DEPLOY_BASE_URL,
+  base: DEPLOY_BASE_URL,
   plugins: [
     react(),
     tailwindcss(),
